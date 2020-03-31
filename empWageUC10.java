a981328bfaf9e8730357ea10e7b32d379b92708e
Aarti Ahire <61686393+Aarti98765@users.noreply.github.com>
2020-03-31 21:23:28 +0530

    REFACTORED: Removed the spaces

--- empWageUC10.java
+++ empWageUC10.java
@@ -1,49 +1,49 @@
 import java.util.*;
 import java.util.Scanner;
+//CREATED THE INTERFACE
 interface IempWageBuilder {
 	public void empWageComputation();
 	public void displayEmpWage();
 }
-
+//CREATED CLASS EMPWAGEUC10
 public class empWageUC10 {
 	public static final int IS_PART_TIME = 1;
 	public static final int IS_FULL_TIME = 2;
 	public static final int COMPANIES_COUNT = 3;
 	public void empWageComputation(companyEmpWage company) {
-		   int empHrs = 0;
+		int empHrs = 0;
 	   	int empWage = 0;
 	   	int totalWage = 0;
 	   	int totalEmpHrs = 0;
-	 	  	int totalWorkingDays = 0;
-			while (totalEmpHrs <= company.getMonthMaxHrs() && 
-           	   totalWorkingDays < company.getWorkingDaysEmp()) { 
-					int empCheck = (int) Math.floor(Math.random() * 10) % 3;
-					totalWorkingDays++;
-					totalEmpHrs++;
-					switch ( empCheck ) {
-	        			case IS_FULL_TIME :
-								empHrs = 8;
-								break; 
-		        		case IS_PART_TIME:
-			      			empHrs = 4;
-								break;
-			   		default :
-								empHrs= 0;
-					}
-					empWage = empHrs * company.getRateOfEmp();
-					totalWage=totalWage+empWage;
-	  		}
-			displayEmpWage(totalWage);
+	 	int totalWorkingDays = 0;
+		while (totalEmpHrs <= company.getMonthMaxHrs() && 
+           	       totalWorkingDays < company.getWorkingDaysEmp()) { 
+			int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+			totalWorkingDays++;
+			totalEmpHrs++;
+			switch ( empCheck ) {
+	        		case IS_FULL_TIME :
+					empHrs = 8;
+					break; 
+		        	case IS_PART_TIME:
+			      		empHrs = 4;
+					break;
+				default :
+					empHrs= 0;
+			}
+			empWage = empHrs * company.getRateOfEmp();
+			totalWage=totalWage+empWage;
+	  	}
+		displayEmpWage(totalWage);
 	}
-
 	public void displayEmpWage(int totalWage) {
 	  		System.out.println("Company");
-      	System.out.println("Total wage of employees:" + totalWage);
+      			System.out.println("Total wage of employees:" + totalWage);
 			System.out.println("");
-    }
-
-   public static void main(String args[]) {
-		   companyEmpWage company[] = new companyEmpWage [COMPANIES_COUNT];
+    	}
+	//MAIN DRIVER PROGRAM
+   	public static void main(String args[]) {
+		companyEmpWage company[] = new companyEmpWage [COMPANIES_COUNT];
    		for (int index=0; index<COMPANIES_COUNT; index++) {
          		Scanner scan = new Scanner(System.in);
          		System.out.println("Enter maximum hours of working in month for a company:");
@@ -53,37 +53,28 @@ public class empWageUC10 {
          		System.out.println("Enter per hour rate of employee:");
          		int empRateHr= scan.nextInt();
          		company[index] = new companyEmpWage(maxHrMonth, numOfWorkDays, empRateHr);
-        			empWageComputation(company[index]);
-  		   } 
-    }
+        		empWageComputation(company[index]);
+  		} 
+    	}
 }
-
+//CREATED CLASS COMPANYEMPWAGE
 class companyEmpWage {
 	private int  maxHrsInMonth = 0;
-   private int  numWorkingDays = 0;
-   private int  empRatePerHr  = 0;
+   	private int  numWorkingDays = 0;
+   	private int  empRatePerHr  = 0;
 	companyEmpWage(int maxHrsMonth, int numOfWorkingDays, int empRatePerHrs) {
-         maxHrsInMonth = maxHrsMonth;
-         numWorkingDays = numOfWorkingDays;
-         empRatePerHr = empRatePerHrs;    
-   } 	 
-	
+        	maxHrsInMonth = maxHrsMonth;
+      		numWorkingDays = numOfWorkingDays;
+         	empRatePerHr = empRatePerHrs;    
+   	} 	 
 	public int getRateOfEmp() {
-
-			return maxHrsInMonth;
-
+		return maxHrsInMonth;
 	}
-
 	public int getWorkingDaysEmp() {
-
-			return numWorkingDays;
-
+		return numWorkingDays;
 	}
-
 	public int getMonthMaxHrs() {
-
-			return empRatePerHr;
-
+		return empRatePerHr;
 	}
 }
 
