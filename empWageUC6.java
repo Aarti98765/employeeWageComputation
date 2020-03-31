e8d68919fa8afd9b670ff61781d212e608efe2e9
Aarti Ahire <61686393+Aarti98765@users.noreply.github.com>
2020-03-31 21:47:38 +0530

    Update empWageUC6.java

--- empWageUC6.java
+++ empWageUC6.java
@@ -6,8 +6,7 @@ public class empWageWhile {
    	public static final int NUM_OF_WORKING_DAYS = 20;
 	public static final int MAX_HRS_IN_MONTH = 100;
 	//MAIN METHOD
-	public static void main(String[] args)
-	{  
+	public static void main(String[] args) {  
       		int empHrs = 0;
 		int empWage = 0;
 		int totalWage = 0;
