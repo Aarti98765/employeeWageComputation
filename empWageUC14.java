924b5b0628bfe86d1e50ba92ea218a88725ccf0d
Aarti98765 <aartiahireengit@gmail.com>
2020-03-31 20:22:46 +0530

    ADDED: Computed employee wage by createting get total wage method

---
+++ empWageUC14.java
@@ -0,0 +1,88 @@
+import java.util.*;
+import java.util.Scanner;
+
+interface IempWageBuilder {
+	void empWageComputation();
+}
+public class empWageUC14 implements IempWageBuilder {
+	public static final int IS_PART_TIME = 1;
+	public static final int IS_FULL_TIME = 2; 
+	ArrayList <companyEmpWage> company = new ArrayList<companyEmpWage>();
+	ArrayList <Integer> dailyWage = new ArrayList<Integer>();
+   public void empWageComputation() {
+		for ( companyEmpWage empObj : company) {
+		   int empHrs = 0;
+	   	int empWage = 0;
+	   	int totalWage = 0;
+	   	int totalEmpHrs = 0;
+	 	  	int totalWorkingDays = 0;
+			while (totalEmpHrs <= empObj.getMonthMaxHrs() && 
+           	   totalWorkingDays < empObj.getWorkingDaysEmp()) { 
+					int empCheck = (int) Math.floor(Math.random() * 10) % 3;
+					totalWorkingDays++;
+					totalEmpHrs++;
+					switch ( empCheck ) {
+	        			case IS_FULL_TIME :
+								empHrs = 8;
+								break; 
+		        		case IS_PART_TIME:
+			      			empHrs = 4;
+								break;
+			   		default :
+								empHrs= 0;
+					}
+					empWage = empHrs * empObj.getRateOfEmp();
+					dailyWage.add(empWage);
+					totalWage=totalWage+empWage;
+	  		}
+         getTotalWage(totalWage);
+	  }
+	}
+	public static void getTotalWage(int totalWage) {
+			System.out.println("Company");
+			System.out.println("Total wage of company:" + totalWage);
+   }
+   public static void main(String args[]) {
+   		empWageUC14 employee = new empWageUC14();
+			Scanner scan = new Scanner(System.in);
+			System.out.println("Enter company count:");
+			int companyCount = scan.nextInt();
+			for (int index=0; index<companyCount; index++) {
+					System.out.println("Enter maximum hours of working in month for a company:");
+         		int maxHrMonth = scan.nextInt();
+         		System.out.println("Enter number of days of working for a company:");
+         		int numOfWorkDays = scan.nextInt();
+         		System.out.println("Enter per hour rate of employee:");
+         		int empRateHr= scan.nextInt();
+         		employee.company.add(new companyEmpWage(maxHrMonth, numOfWorkDays, empRateHr));
+  		   } 
+			employee.empWageComputation();
+   }
+}
+
+class companyEmpWage {
+	private int  maxHrsInMonth = 0;
+   private int  numWorkingDays = 0;
+   private int  empRatePerHr  = 0;
+	companyEmpWage(int maxHrsMonth, int numOfWorkingDays, int empRatePerHrs) {
+         maxHrsInMonth = maxHrsMonth;
+         numWorkingDays = numOfWorkingDays;
+         empRatePerHr = empRatePerHrs;    
+   } 	 
+	public int getRateOfEmp() {
+
+		   return maxHrsInMonth;
+
+	}
+	public int getWorkingDaysEmp() {
+
+			return numWorkingDays;
+
+	}
+	public int getMonthMaxHrs() {
+
+			return empRatePerHr;
+
+	}
+}
+
